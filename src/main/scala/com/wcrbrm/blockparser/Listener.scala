982c4c79f45ae959d092f8ac4024d4729f86e415
wcrbrm <viclopata@gmail.com>
2018-12-26 18:52:10 +0200

    clear listener

--- src/main/scala/com/wcrbrm/blockparser/Listener.scala
+++ src/main/scala/com/wcrbrm/blockparser/Listener.scala
@@ -3,7 +3,10 @@ package com.wcrbrm.blockparser
 import okhttp3._
 import io.circe._
 
-case class Op(value: String) {
+trait ConvertedToJson {
+  def toJson: String
+}
+case class Op(value: String) extends ConvertedToJson {
   def toJson: String = Json.fromFields(List(("op", Json.fromString(value)))).noSpaces
 }
 object OpPing extends Op("ping")
@@ -13,14 +16,18 @@ object OpPingBlock extends Op("ping_block")
 object OpPingTx extends Op("ping_tx")
 
 object BlockchainInfoListener extends WebSocketListener {
+
+  def deliver(webSocket: WebSocket, packet: ConvertedToJson) {
+    val str: String = packet.toJson
+    println("sending " + str)
+    webSocket.send(str)
+  }
+
   override def onOpen(webSocket: WebSocket, response: Response) {
     println("WebSocket connected")
     println("Sending ping message")
-
-    println("(sending) " + OpPing.toJson)
-    webSocket.send(OpPing.toJson)
-    println("(sending) " + OpBlocksSub.toJson)
-    webSocket.send(OpBlocksSub.toJson)
+    deliver(webSocket, OpPing)
+    deliver(webSocket, OpBlocksSub)
   }
   override def onMessage(webSocket: WebSocket, text: String) {
     println("Receiving : " + text)
