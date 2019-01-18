ef83fa42c00be818fda8fa486d980d57c911950b
wcrbrm <viclopata@gmail.com>
2019-01-18 14:59:35 +0200

    more testing of actors interaction

---
+++ src/test/scala/com/wcrbrm/blockparser/ImplicitSenderSpec.scala
@@ -0,0 +1,23 @@
+package com.wcrbrm.blockparser
+
+import akka.actor.ActorSystem
+import akka.testkit.{ ImplicitSender, TestActors, TestKit }
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
+
+class ImplicitSenderSpec extends TestKit(ActorSystem("IsSpec")) with ImplicitSender
+  with WordSpecLike with Matchers with BeforeAndAfterAll {
+
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "An Echo actor" must {
+
+    "send back messages unchanged" in {
+      val echo = system.actorOf(TestActors.echoActorProps)
+      echo ! "hello world"
+      expectMsg("hello world")
+    }
+
+  }
+}
--- src/test/scala/com/wcrbrm/blockparser/ParentWorkerActorSpec.scala
+++ src/test/scala/com/wcrbrm/blockparser/ParentWorkerActorSpec.scala
@@ -1,19 +1,19 @@
 package com.wcrbrm.blockparser
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import akka.pattern.ask
 
 import scala.concurrent.duration._
-import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import akka.testkit.{ImplicitSender, TestKit}
 import com.wcrbrm.blockparser.ParentActor.CreateChildMsg
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-object WorkerActor {
-  def props(id: String): Props = Props(new WorkerActor(id))
+object ChildActor {
+  def props(id: String): Props = Props(new ChildActor(id))
 }
-class WorkerActor(id: String) extends Actor with ActorLogging {
-  override def preStart(): Unit = log.info(s"WorkerActor ${id} started")
-  override def postStop(): Unit = log.info("WorkerActor  ${id}stopped")
+class ChildActor(id: String) extends Actor with ActorLogging {
+  override def preStart(): Unit = log.debug(s"WorkerActor ${id} started")
+  override def postStop(): Unit = log.debug(s"WorkerActor ${id} stopped")
   // import WorkerActor._
   def receive = {
     case "ping" =>
@@ -23,6 +23,7 @@ class WorkerActor(id: String) extends Actor with ActorLogging {
         throw new Exception("Sender is detected as dead letters")
 
       log.info(" attempt to properly detect sender={}", senderStr)
+      sender ! "pong"
   }
 }
 
@@ -32,32 +33,41 @@ object ParentActor {
     final case object CreateChildMsg
 }
 class ParentActor extends Actor with ActorLogging {
-    override def preStart(): Unit = log.info("ParentActor started")
-    override def postStop(): Unit = log.info("ParentActor stopped")
+    override def preStart(): Unit = log.debug("ParentActor started")
+    override def postStop(): Unit = log.debug("ParentActor stopped")
 
     import ParentActor._
+    implicit val timeoutOnAsk = akka.util.Timeout(5.seconds) // timeout on actor ask
+
+    var creator: Option[ActorRef] = None
     def receive = {
       case CreateChildMsg =>
-        val id = "12312312312"
-        log.info(s"attempt to create child ${id}")
-        val actor = context.actorOf(WorkerActor.props(id), s"worker${id}")
-        actor ! "ping"
+        val id = "1"
+        creator = Some(sender)
+        log.info(s"attempt to create child ${id}, initiated by {}", sender)
+        val childActor = context.actorOf(ChildActor.props(id), s"worker${id}")
+        childActor ! "ping"
+
+      case s: String =>
+        log.info(s"parent received string '{}' from {}", s, sender)
+        creator.map { c =>
+          log.info("sending 'done' to parent {}", c)
+          c ! "done"
+        }
     }
 }
 
-class MySpec() extends TestKit(ActorSystem("MySpec")) with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll {
+class MySpec() extends TestKit(ActorSystem("My")) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
 
     override def afterAll: Unit = {
         TestKit.shutdownActorSystem(system)
     }
-    implicit val timeoutOnAsk = akka.util.Timeout(30.minutes) // timeout on actor ask
 
     "Parent actor" must {
         "create another actor" in {
             val echo = system.actorOf(Props[ParentActor], "parent")
-            echo ? CreateChildMsg
-            // expectMsg("hello world")
+            echo ! CreateChildMsg
+            expectMsg("done")
         }
 
     }
